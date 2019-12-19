82edd93976e259b62b645a1e2d4eb7315438194c
unknown <albacursowebutrera@gmail.com>
2019-12-19 11:15:30 +0100

    Primera versión: Proyecto Búsqueda

---
+++ busqueda.js
@@ -0,0 +1,22 @@
+function buscar(){
+
+    var palabra=document.getElementById("palabra").value;
+
+    if (palabra == "verde"){
+        document.getElementById("mensaje").value = "Palabra encontrada"; 
+    }
+    else if (palabra == "rojo"){
+        document.getElementById("mensaje").value = "Palabra encontrada"; 
+    }
+    else if (palabra == "azul"){
+        document.getElementById("mensaje").value = "Palabra encontrada"; 
+    }
+    else{
+        document.getElementById("mensaje").value = "Palabra NO encontrada";
+    }
+
+}
+
+function salir(){
+    window.close();
+}
